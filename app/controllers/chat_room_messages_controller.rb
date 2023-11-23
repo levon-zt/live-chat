7581de0a3ccd5087b23c53aa0583a4e37ec24111
Levon Arakelyan <levon_a@zealous.tech>
2023-11-23 11:22:41 +0400

    Realy bad commit message

--- app/controllers/chat_room_messages_controller.rb
+++ app/controllers/chat_room_messages_controller.rb
@@ -16,16 +16,18 @@ class ChatRoomMessagesController < AuthenticationController
       return
     end
 
-    chat_message_author = chat_room.chat_room_users.find_by(user_id: message_author_id)
+    chat_message_author = chat_room.chat_room_users.find_by(user_id: Current.user.id)
     chat_room_message = ChatRoomMessage.new(message: message, chat_room_user_id: chat_message_author.id)
 
     if chat_room_message.save
       chat_room_message.broadcast_append_later_to(
         "chat_room_messages",
-        locals: {
+        target: "chat_room_messages",
+        partial: "chat_room_messages/chat_room_message",
+        locals: { 
           chat_room_message: chat_room_message,
-          chat_room_id: chat_room.id,
-          is_user_message_owner: chat_room_message.chat_room_user.user_id == message_author_id
+          message_author_id: message_author_id,
+          chat_room_id: chat_room.id
         }
       )
     end
--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -1,14 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["messagesContainer"];
+  static targets = ["message"];
 
   connect() {
     this.scrollDown();
   }
 
   scrollDown() {
-    const elem = this.messagesContainerTarget;
-    elem.scrollTo(0, elem.scrollHeight);
+    const elem = this.messageTarget.parentElement.parentElement
+    elem.scrollTo(0, 100000);
   }
 }
